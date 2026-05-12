6519a87eb9538c419fd1d8752d13e03760a29bd9
RMSROCGMC <rmsgithub@gmail.com>
2026-05-13 07:15:51 +0800

    Update quiz_list.js

--- quiz_list.js
+++ quiz_list.js
@@ -1,4 +1,5 @@
 window.allQuizzes = [
     { name: "功能測試測驗", code: "test01" },
-    { name: "國防科複習題1140202", code: "ND-1140202-E102" }
+    { name: "國防科複習題1140202", code: "ND-1140202-E102" },
+    { name: "數學練習卷AI出題測驗", code: "MH-1140202-E1B" } 
 ];
